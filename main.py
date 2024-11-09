6e02760afef0d9e22480b7dbb086ddb92d3d88f2
Joshua Chen <jzc0289@auburn.edu>
2024-11-09 02:08:45 -0600

    add typing cooldown

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import pyperclip
+import time
+from pynput.keyboard import Controller, Key
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def paste(toPaste):
+    for char in toPaste:
+        # print(char)
+        keyboard.tap(char)
+        time.sleep(0.025)
 
+if __name__ == "__main__":
+    keyboard = Controller()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    toPaste = pyperclip.paste()
+    pyperclip.copy(toPaste)
+    # print(toPaste)
+    time.sleep(5)
+    paste(toPaste)
